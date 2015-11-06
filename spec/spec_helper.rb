09b38294c2853657d55bd85fce0e19413740a2e9
Rachid Calazans <rachidcalazans@gmail.com>
2015-11-06 15:05:39 -0300

    create the scpec_helper
    - include the require imports;
    - add a connection with a sqlite db;
    - configure the rspec with factory_girl and databse_cleaner;
    - create the schema.rb to generate the tables.
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

---
+++ spec/spec_helper.rb
@@ -0,0 +1,26 @@
+require 'active_record'
+require 'ar'
+require 'database_cleaner'
+require 'factory_girl'
+
+ActiveRecord::Base.establish_connection(adapter:  'sqlite3',
+                                        database: "#{File.dirname(__FILE__)}/ar.sqlite3")
+
+load File.dirname(__FILE__) + '/support/schema.rb'
+
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+  FactoryGirl.find_definitions
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+end
