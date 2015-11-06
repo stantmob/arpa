99461d38491261ae0119d244e256908b06157235
Rachid Calazans <rachidcalazans@gmail.com>
2015-11-06 18:43:11 -0300

    Add implementation for ResourceValidator
    - add tests for that validations.
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- lib/ar/validators/resource_validator.rb
+++ lib/ar/validators/resource_validator.rb
@@ -1,6 +1,16 @@
 module Ar
   module Validators
     class ResourceValidator
+      include ActiveModel::Validations
+
+      attr_reader :full_name, :name
+
+      validates :full_name, :name, presence: true
+
+      def initialize(resource)
+        @full_name = resource.full_name
+        @name      = resource.name
+      end
 
     end
   end
--- spec/lib/ar/validators/resource_validator_spec.rb
+++ spec/lib/ar/validators/resource_validator_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe Ar::Validators::ResourceValidator do
+  let(:resource) { double full_name: '', name: '' }
+
+  subject { Ar::Validators::ResourceValidator.new resource }
+
+  before { subject.valid? }
+
+  context 'when require' do
+    it { expect(subject.errors).to be_added(:full_name, :blank) }
+    it { expect(subject.errors).to be_added(:name, :blank) }
+  end
+
 end
