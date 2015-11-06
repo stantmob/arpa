8555c3b460adabf0a83f3d6d76b016f2c8a24d43
Rachid Calazans <rachidcalazans@gmail.com>
2015-11-06 17:20:44 -0300

    Add implementation for Action entity
    - create initialize method and your attrs_reader;
    - create the description method with test.
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- lib/ar/entities/action.rb
+++ lib/ar/entities/action.rb
@@ -1,6 +1,22 @@
 module Ar
   module Entities
     class Action
+
+      attr_reader :resource, :name, :created_at, :updated_at
+
+      def initialize(attrs = {})
+        attrs = attrs.with_indifferent_access
+
+        @resource    = attrs[:resource]
+        @name        = attrs[:name]
+        @created_at  = attrs[:created_at]
+        @updated_at  = attrs[:updated_at]
+      end
+
+      def description
+        I18n.t(name.to_sym, scope: 'entities.action.description', resource_name: resource.name)
+      end
+
     end
   end
 end
--- spec/lib/ar/entities/action_spec.rb
+++ spec/lib/ar/entities/action_spec.rb
@@ -1,4 +1,19 @@
 require 'spec_helper'
 
 describe Ar::Entities::Action do
+  let(:resource) { double(name: 'users') }
+
+  subject { Ar::Entities::Action.new resource: resource, name: 'index'}
+
+  describe 'getting description by action name and resource name' do
+
+    context 'when resource.name is "users" and action.name is "index"' do
+
+      it 'description should be "List of users"' do
+        expect(subject.description).to eql "List of users"
+      end
+    end
+
+  end
+
 end
