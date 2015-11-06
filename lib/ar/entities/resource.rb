e16029d22872463f7e0c7291264d4e72e4fbc255
Rachid Calazans <rachidcalazans@gmail.com>
2015-11-06 16:27:37 -0300

    implement the resource entity
    - create tests for initialize method.
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- lib/ar/entities/resource.rb
+++ lib/ar/entities/resource.rb
@@ -2,6 +2,52 @@ module Ar
   module Entities
     class Resource
 
+      attr_reader :full_name, :name
+
+      def initialize(attrs = {})
+        @full_name = attrs[:full_name]
+
+        build_name
+      end
+
+      private
+
+      def build_name
+        name  = remove_word(full_name)
+        @name = adjust_name(name)
+      end
+
+      def adjust_name(name)
+        parts_of_name   = name.split '::'
+        refactored_name = String.new
+
+        parts_of_name.each_with_index do |part, index|
+          refactored_name << '/' if index > 0
+          refactored_name << change_to_snake_case(part)
+        end
+
+        refactored_name
+      end
+
+      def remove_word(word, word_to_delete = 'Controller')
+        word = "#{word}del" if word.include?(word_to_delete)
+        word.slice!("#{word_to_delete}del")
+        word
+      end
+
+      def change_to_snake_case(name)
+        parts_of_name   = name.split(/(?=[A-Z])/) # Split at CamelCase
+        refactored_name = String.new
+
+        parts_of_name.each_with_index do |part, index|
+          part.downcase!
+          refactored_name << '_' if index > 0
+          refactored_name << part
+        end
+
+        refactored_name
+      end
+
     end
   end
 end
--- spec/lib/ar/entities/resource_spec.rb
+++ spec/lib/ar/entities/resource_spec.rb
@@ -1,4 +1,33 @@
 require 'spec_helper'
 
 describe Ar::Entities::Resource do
+
+  describe 'initializing' do
+
+    context 'when pass :full_name as "UsersController"' do
+      subject { Ar::Entities::Resource.new full_name: 'UsersController' }
+
+      it 'full_name should be "UsersController"' do
+        expect(subject.full_name).to eql 'UsersController'
+      end
+
+      it 'name should be builded as "users"' do
+        expect(subject.name).to eql 'users'
+      end
+    end
+
+    context 'when pass :full_name as "Authentication::Someone::UsersController"' do
+      subject { Ar::Entities::Resource.new full_name: 'Authentication::Someone::UsersController' }
+
+      it 'full_name should be "Authentication::Someone::UsersController"' do
+        expect(subject.full_name).to eql 'Authentication::Someone::UsersController'
+      end
+
+      it 'name should be builded as "authentication/someone/users"' do
+        expect(subject.name).to eql 'authentication/someone/users'
+      end
+    end
+
+  end
+
 end
