75eb952bfba91121260aad3cac655622a3d3ada6
Rachid Calazans <rachidcalazans@gmail.com>
2015-11-06 17:26:06 -0300

    Create the Profile entity
    - add require import onto ar.rb;
    - create Profile entity class with your attributes.
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- lib/ar.rb
+++ lib/ar.rb
@@ -2,6 +2,7 @@ require "ar/version"
 require "ar/entities/resource"
 require "ar/entities/action"
 require "ar/entities/role"
+require "ar/entities/profile"
 
 module Ar
   I18n.config.enforce_available_locales = true
---
+++ lib/ar/entities/profile.rb
@@ -0,0 +1,19 @@
+module Ar
+  module Entities
+    class Profile
+
+      attr_reader :name, :description, :created_at, :updated_at, :removed
+
+      def initialize(attrs = {})
+        attrs = attrs.with_indifferent_access
+
+        @name        = attrs[:name]
+        @description = attrs[:name]
+        @created_at  = attrs[:created_at]
+        @updated_at  = attrs[:updated_at]
+        @removed     = attrs[:removed]
+      end
+
+    end
+  end
+end
