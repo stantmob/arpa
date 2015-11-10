d119cd0fcb7e8cc642703deff6d7d054979e6efa
Rachid Calazans <rachidcalazans@gmail.com>
2015-11-10 09:49:57 -0300

    Update ResourceManagerCreator
    - implement the create method to create resources and your actions;
    - create tests for that create method.
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- lib/ar/services/resources/resource_manager_creator.rb
+++ lib/ar/services/resources/resource_manager_creator.rb
@@ -2,6 +2,31 @@ module Ar
   module Services
     module Resources
       class ResourceManagerCreator
+        include Ar::Services::Base
+
+        def create(params, callback)
+          manager_action callback do
+            params[:resourceables].collect do |resourceable|
+              resource = resource_creator.create(resourceable)
+              action_params = action_params(resource, resourceable)
+              action_creator.create_many(action_params)
+
+              resource
+            end
+          end
+        end
+
+        private
+
+        def resource_creator
+        end
+
+        def action_creator
+        end
+
+        def action_params(resource, resourceable)
+          {resource: resource, actions_names: resourceable.actions_methods}
+        end
 
       end
     end
--- spec/lib/ar/services/resources/resource_manager_creator_spec.rb
+++ spec/lib/ar/services/resources/resource_manager_creator_spec.rb
@@ -2,4 +2,47 @@ require 'spec_helper'
 
 describe Ar::Services::Resources::ResourceManagerCreator, type: :service, fast: true do
 
+  let(:resource_creator) { double }
+  let(:action_creator)   { double }
+  let(:element)          { double }
+  let(:params)           { {resourceables: [resourceable_001, resourceable_002]} }
+  let(:success_proc)     { ->(r) {} }
+  let(:callback)         { {success: success_proc, fail: ->(e) {raise e} } }
+
+  let(:resourceable_001) { double actions_methods: [] }
+  let(:resourceable_002) { double actions_methods: []}
+  let(:resource_created) { double }
+
+  let(:setup_creators) do
+    allow(subject).to receive(:resource_creator).and_return(resource_creator)
+    allow(subject).to receive(:action_creator).and_return(action_creator)
+  end
+
+  let(:setup_creators_methods) do
+    allow(resource_creator).to receive(:create).and_return(resource_created)
+    allow(action_creator).to receive(:create_many)
+  end
+
+  let(:result) { subject.create params, callback }
+
+  before do
+    setup_creators
+    setup_creators_methods
+    allow(success_proc).to receive(:call)
+    result
+  end
+
+  it 'resource_creator should call :create with :resourceable as parameter' do
+    expect(resource_creator).to have_received(:create).with(resourceable_001).once
+    expect(resource_creator).to have_received(:create).with(resourceable_002).once
+  end
+
+  it 'action_creator should call :create_many with :action_params as parameter' do
+    expect(action_creator).to have_received(:create_many).with({resource: resource_created, actions_names: []}).twice
+  end
+
+  it 'success callback should be called with Array of resources as parameter' do
+    expect(success_proc).to have_received(:call).with([resource_created, resource_created])
+  end
+
 end
