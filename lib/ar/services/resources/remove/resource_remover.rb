e1ca4e3ae086c013391522cf9b4bb9e2b204c10a
Rachid Calazans <rachidcalazans@gmail.com>
2015-11-13 17:17:53 -0300

    Update the ResourceRemover Service
    - implement the remove_nonexistent_resources method;
    - create tests for that method.
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- lib/ar/services/resources/remove/resource_remover.rb
+++ lib/ar/services/resources/remove/resource_remover.rb
@@ -5,6 +5,23 @@ module Ar
         class ResourceRemover
 
           def remove_nonexistent_resources(resourceables)
+            resourceables_names = resourceables.map { |r| r.to_s }
+
+            finder_repo.all.each do |resource|
+              exist = resourceables_names.include?(resource.full_name)
+              remover_repo.destroy(resource) unless exist
+            end
+
+          end
+
+          private
+
+          def finder_repo
+            @finder_repo ||= Ar::Repositories::Resources::Finder.new
+          end
+
+          def remover_repo
+            @remover_repo ||= Ar::Repositories::Resources::Remover.new
           end
 
         end
---
+++ spec/lib/ar/services/resources/remove/resource_remover_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe Ar::Services::Resources::Remove::ResourceRemover, type: :service, fast: true do
+
+  let(:resource_001) { double full_name: 'UsersController' }
+  let(:resource_002) { double full_name: 'OldContactsController' }
+  let(:resources)    { [resource_001, resource_002]}
+
+  let(:resourceable_001) { double to_s: 'UsersController' }
+  let(:resourceable_002) { double to_s: 'ContactsController' }
+  let(:resourceables)    { [resourceable_001, resourceable_002]}
+
+  describe 'removing nonexistent resources' do
+
+    let(:finder_repo_class) { Ar::Repositories::Resources::Finder }
+    let(:finder_repo)       { instance_double finder_repo_class }
+
+    let(:remover_repo_class) { Ar::Repositories::Resources::Remover }
+    let(:remover_repo)       { instance_double remover_repo_class }
+
+    before do
+      allow(finder_repo_class).to receive(:new).and_return(finder_repo)
+      allow(finder_repo).to receive(:all).and_return(resources)
+
+      allow(remover_repo_class).to receive(:new).and_return(remover_repo)
+      allow(remover_repo).to receive(:destroy).with(resource_002)
+
+      subject.remove_nonexistent_resources(resourceables)
+    end
+
+    it 'finder repository should call :new once' do
+      expect(finder_repo_class).to have_received(:new).once
+    end
+
+    it 'remover repository should call :new once' do
+      expect(remover_repo_class).to have_received(:new).once
+    end
+
+    it 'finder repository should call :all once' do
+      expect(finder_repo).to have_received(:all).once
+    end
+
+    it 'remover repository should call :destroy once' do
+      expect(remover_repo).to have_received(:destroy).once
+      expect(remover_repo).to have_received(:destroy).with(resource_002).once
+    end
+
+  end
+
+end
