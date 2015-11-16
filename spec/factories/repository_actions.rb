a287c9c4791ede2c96689817ab0fef270defaee2
Rachid Calazans <rachidcalazans@gmail.com>
2015-11-16 11:05:50 -0300

    Add RepositoryActions factory
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

---
+++ spec/factories/repository_actions.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  factory :repository_action, class: Ar::Repositories::Actions::RepositoryAction do
+
+    trait :index do
+      name 'index'
+      resource { FactoryGirl.create(:repository_resource, :user) }
+    end
+
+    trait :show do
+      name 'show'
+      resource { FactoryGirl.create(:repository_resource, :user) }
+    end
+
+  end
+end
