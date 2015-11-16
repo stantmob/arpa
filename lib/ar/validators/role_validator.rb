71e20b8d7b1c1da0d1d3b89cf2047727beadd52a
Rachid Calazans <rachidcalazans@gmail.com>
2015-11-16 17:18:54 -0300

    Update RoleValidator
    - add new validator and new attribute;
    - add new tests for that new validation.
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- lib/ar/validators/role_validator.rb
+++ lib/ar/validators/role_validator.rb
@@ -3,13 +3,17 @@ module Ar
     class RoleValidator
       include ActiveModel::Validations
 
-      attr_reader :name, :description
+      attr_reader :name, :description, :action_ids
 
       validates :name, :description, presence: true
+
+      validates :action_ids, presence: { message: :many_blank }
+
 
       def initialize(role)
         @name        = role.name
         @description = role.description
+        @action_ids  = role.action_ids
       end
 
     end
--- spec/lib/ar/validators/role_validator_spec.rb
+++ spec/lib/ar/validators/role_validator_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Ar::Validators::RoleValidator, type: :validator, fast: true do
-  let(:role) { double name: '', description: '' }
+  let(:role) { double name: '', description: '', action_ids: '' }
 
   subject { Ar::Validators::RoleValidator.new role }
 
@@ -10,6 +10,7 @@ describe Ar::Validators::RoleValidator, type: :validator, fast: true do
   context 'when require' do
     it { expect(subject.errors).to be_added(:name, :blank) }
     it { expect(subject.errors).to be_added(:description, :blank) }
+    it { expect(subject.errors).to be_added(:action_ids, :many_blank) }
   end
 
 end
