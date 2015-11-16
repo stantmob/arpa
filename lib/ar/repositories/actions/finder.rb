11dde2b1d7344aa077b7a7c82a4b0f0c0520317f
Rachid Calazans <rachidcalazans@gmail.com>
2015-11-16 15:13:32 -0300

    Update Repositories::Actions::Finder
    - implement the by_name_and_resoruce method;
    - create tests for that method.
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- lib/ar/repositories/actions/finder.rb
+++ lib/ar/repositories/actions/finder.rb
@@ -5,6 +5,8 @@ module Ar
         include Ar::Repositories::Base
 
         def by_name_and_resource(name, resource)
+          record = repository_class.where(name: name, repository_resource_id: resource.id).first
+          mapper_instance.map_to_entity(record, Ar::Entities::Action.new) if record
         end
 
         def mapper_instance
---
+++ spec/lib/ar/repositories/actions/finder_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe Ar::Repositories::Actions::Finder, type: :repository, slow: true do
+
+  let(:resource_record)   { create :repository_resource, :user }
+  let(:action_record_001) { create :repository_action, :index, resource: resource_record }
+  let(:action_record_002) { create :repository_action, :show, resource: resource_record}
+
+  before do
+    action_record_001
+    action_record_002
+  end
+
+  describe '#by_name_and_resource' do
+
+    context 'when exist with the params' do
+
+      let(:result) { subject.by_name_and_resource('index', resource_record) }
+
+      it 'should return a resource with name "index"' do
+        expect(result.name).to eql 'index'
+      end
+
+      it 'the result should be an instance of Ar::Entities::Action' do
+        expect(result).to be_an Ar::Entities::Action
+      end
+    end
+
+    context 'when nonexist with the params' do
+
+      let(:result) { subject.by_name_and_resource('nonexist_action', resource_record) }
+
+      it 'the result should return nil' do
+        expect(result).to be_nil
+      end
+    end
+
+  end
+
+end
