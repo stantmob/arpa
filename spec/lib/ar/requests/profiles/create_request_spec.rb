e4f11e18c0d928c8ecaa0b06cb575ef2d1d7718a
Rachid Calazans <rachidcalazans@gmail.com>
2015-11-18 11:26:21 -0300

    Add CreateRequest test
    - add integration test for create a new Profile.
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

---
+++ spec/lib/ar/requests/profiles/create_request_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+RSpec.describe "Create Profile", type: [:request, :profile] do
+  let(:finder_profile_repo)      { Ar::Repositories::Profiles::Finder.new }
+  let(:finder_resource_repo)     { Ar::Repositories::Resources::Finder.new }
+  let(:finder_role_repo)         { Ar::Repositories::Roles::Finder.new }
+  let(:creator_resource_service) { Ar::Services::Resources::ResourceManagerCreator.new }
+  let(:creator_role_service)     { Ar::Services::Roles::RoleManagerCreator.new }
+  let(:creator_profile_service)  { Ar::Services::Profiles::ProfileManagerCreator.new }
+  let(:success_proc)             { ->(r) {} }
+  let(:callback)                 { {success: success_proc, fail: ->(e) {raise e} } }
+
+  context 'when have no created resources or actions' do
+    let(:resourceable_001)  { double to_s: 'UsersController', actions_methods: [:index, :show, 'edit'] }
+    let(:resourceable_002)  { double to_s: 'ContactsController', actions_methods: [:index, 'edit'] }
+    let(:resourceables)     { [resourceable_001, resourceable_002] }
+    let(:created_resources) { creator_resource_service.create({resourceables: resourceables}, callback) }
+
+    let(:action_ids) { finder_resource_repo.all.first.actions.ids }
+    let(:role_001_params)   { {role: {name: 'role_name_001', description: 'role_description_001', action_ids: action_ids}} }
+    let(:role_002_params)   { {role: {name: 'role_name_002', description: 'role_description_002', action_ids: action_ids}} }
+    let(:created_roles) do
+      creator_role_service.create(role_001_params, callback)
+      creator_role_service.create(role_002_params, callback)
+    end
+
+    let(:role_ids) { finder_role_repo.all.map { |r| r.id } }
+
+    before do
+      created_resources
+      created_roles
+      profile_params = {profile: {name: 'profile_name', description: 'profile_description', role_ids: role_ids}}
+      creator_profile_service.create(profile_params, callback)
+    end
+
+    it 'should be recorded one profile' do
+      expect(finder_profile_repo.all.size).to be == 1
+    end
+
+    it 'should has 2 roles associated' do
+      expect(finder_profile_repo.all.first.roles.size).to be == 2
+    end
+
+    context 'names of associated roles' do
+      it {expect(finder_profile_repo.all.first.roles.first.name).to eql 'role_name_001'}
+      it {expect(finder_profile_repo.all.first.roles.second.name).to eql 'role_name_002'}
+    end
+
+  end
+
+end
