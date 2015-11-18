3da93b53d60acc4ce16e860787f08a6b1c425d63
Rachid Calazans <rachidcalazans@gmail.com>
2015-11-18 10:42:33 -0300

    Add RepositoryProfile
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

---
+++ lib/ar/repositories/profiles/repository_profile.rb
@@ -0,0 +1,12 @@
+module Ar
+  module Repositories
+    module Profiles
+      class RepositoryProfile < ActiveRecord::Base
+        default_scope {where(removed: false).order(name: :asc)}
+
+        has_and_belongs_to_many :roles, class_name: 'Ar::Repositories::Roles::RepositoryRole'
+
+      end
+    end
+  end
+end
