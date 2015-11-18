dc7413e3feb6693a52a233b6af9cfb26eeac5b3f
Rachid Calazans <rachidcalazans@gmail.com>
2015-11-18 16:55:47 -0300

    Add Remove Request test for Profile
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

---
+++ spec/lib/ar/requests/profiles/remove_request_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+RSpec.describe "Remove Profile", type: [:request, :profile] do
+  let(:finder_profile_repo)     { Ar::Repositories::Profiles::Finder.new }
+  let(:remover_profile_service) { Ar::Services::Profiles::ProfileManagerRemover.new }
+  let(:success_proc)            { ->(r) {} }
+  let(:callback)                { {success: success_proc, fail: ->(e) {raise e} } }
+
+  describe 'removing a Profile' do
+
+    before do
+      create :repository_profile
+      remover_profile_service.remove(profile_params_to_remove, callback)
+    end
+
+    context 'when need destroy a Profile' do
+      let(:profile_params_to_remove) { {profile: finder_profile_repo.all.first } }
+
+      it 'record should be removed ' do
+        expect(finder_profile_repo.all.size).to be == 0
+      end
+
+    end
+
+    context 'when need disable a Profile' do
+      let(:profile_params_to_remove) { {profile: finder_profile_repo.all.first, disable: true } }
+
+      it 'record should be removed ' do
+        expect(finder_profile_repo.all.size).to be == 0
+      end
+
+    end
+
+  end
+
+end
