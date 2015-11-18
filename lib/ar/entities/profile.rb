083a42b9924861dd8275c06e60a8e3564dbc8fc6
Rachid Calazans <rachidcalazans@gmail.com>
2015-11-18 10:44:19 -0300

    Update Entity Profile
    - add new association attributes;
    - update the repository_role with hbtm with profile repository.
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- lib/ar/entities/profile.rb
+++ lib/ar/entities/profile.rb
@@ -2,7 +2,7 @@ module Ar
   module Entities
     class Profile
 
-      attr_reader :id, :name, :description, :created_at, :updated_at, :removed
+      attr_reader :id, :name, :description, :role_ids, :roles, :created_at, :updated_at, :removed
 
       def initialize(attrs = {})
         attrs = attrs.with_indifferent_access
@@ -10,6 +10,8 @@ module Ar
         @id          = attrs[:id]
         @name        = attrs[:name]
         @description = attrs[:name]
+        @role_ids    = attrs[:role_ids]
+        @roles       = attrs[:roles]
         @created_at  = attrs[:created_at]
         @updated_at  = attrs[:updated_at]
         @removed     = attrs[:removed]
