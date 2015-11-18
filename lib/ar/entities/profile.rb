5db8b30bf348c864895350ce34f8316d61dcb831
Rachid Calazans <rachidcalazans@gmail.com>
2015-11-18 11:25:55 -0300

    Update Profile Entity
    - add default values for some attributes.
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- lib/ar/entities/profile.rb
+++ lib/ar/entities/profile.rb
@@ -10,11 +10,11 @@ module Ar
         @id          = attrs[:id]
         @name        = attrs[:name]
         @description = attrs[:name]
-        @role_ids    = attrs[:role_ids]
-        @roles       = attrs[:roles]
+        @role_ids    = attrs[:role_ids] || []
+        @roles       = attrs[:roles] || []
         @created_at  = attrs[:created_at]
         @updated_at  = attrs[:updated_at]
-        @removed     = attrs[:removed]
+        @removed     = attrs[:removed] || false
       end
 
     end
