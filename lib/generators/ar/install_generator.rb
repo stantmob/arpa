21bab5159068322bb248cf323737ed5e9387afee
Rachid Calazans <rachidcalazans@gmail.com>
2015-11-23 18:38:07 -0300

    Change InstallGenerator class
    - add new generators for views, controller and locales.
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- lib/generators/ar/install_generator.rb
+++ lib/generators/ar/install_generator.rb
@@ -3,15 +3,43 @@ module Ar
     class InstallGenerator < Rails::Generators::Base
       include Rails::Generators::Migration
 
-      source_root File.expand_path("../templates", __FILE__)
+      source_root File.expand_path("..", __FILE__)
 
       def self.next_migration_number(dirname)
         return Time.new.utc.strftime("%Y%m%d%H%M%S") if ActiveRecord::Base.timestamped_migrations
         "%.3d" % (current_migration_number(dirname) + 1)
       end
 
-      def create_migration_file
-        migration_template 'migration.rb', 'db/migrate/create_ar_tables.rb'
+      def create_migrate_files
+        migration_template 'templates/migration.rb', 'db/migrate/create_ar_tables.rb'
+      end
+
+      def copy_resources_views_files
+        copy_file '../../ar/views/resources/index.html.erb', 'app/views/ar/resources/index.html.erb'
+        copy_file '../../ar/views/resources/show.html.erb', 'app/views/ar/resources/show.html.erb'
+      end
+
+      def copy_stylesheet_files
+      end
+
+      def add_controllers
+        copy_file '../../ar/controllers/resources_controller.rb', 'app/controllers/ar/resources_controller.rb'
+      end
+
+      def add_routes
+        route <<-RUBY
+        scope module: :ar do
+          resources :resources do
+            collection do
+            get 'generate_resources_and_actions'
+            end
+          end
+        end
+        RUBY
+      end
+
+      def copy_locales
+        copy_file '../../config/locales/ar.en.yml', 'config/locales/ar.en.yml'
       end
 
     end
