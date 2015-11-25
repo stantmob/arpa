80273a04160182ffdd5b2396cda79849c87c7aee
Rachid Calazans <rachidcalazans@gmail.com>
2015-11-25 12:05:25 -0300

    Refactor DataMappers::Base
    - refactor methods to mapper entity or record;
    - add map associations with correct mapper method call (map_to_record
    or map_to_entity).
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- lib/ar/data_mappers/base.rb
+++ lib/ar/data_mappers/base.rb
@@ -4,16 +4,73 @@ module Ar
       include Singleton
 
       def map_to_record(entity, options={})
+        options[:map_association_to] ||= :map_to_record
         attrs_to_record = self.class._attrs_to_record
         attributes      = attributes_from(entity, attrs_to_record, options)
         self.class._repository_class.new(attributes)
       end
 
       def map_to_entity(record, options={})
+        options[:map_association_to] ||= :map_to_entity
         attrs_to_entity = self.class._attrs_to_entity
         attributes      = attributes_from(record, attrs_to_entity, options)
         self.class._entity_class.new(attributes)
       end
+
+      private
+
+      def attributes_from(object, attrs_to_map, options={})
+        options[:map_association_to] = options.fetch(:map_association_to, :map_to_entity)
+        options[:map_association]    = options.fetch(:map_association, true)
+
+        attrs_to_map ||= self.class._attributes_to_map
+
+        begin
+           build_hash_attributes(attrs_to_map, object, options)
+        rescue => e
+          raise StandardError, "#{self.class} -> #{e.message}"
+        end
+      end
+
+      def build_hash_attributes(attributes_to_map, object, options)
+        builder_attribute = build_hash_attribute.curry.(object, options)
+
+        attributes_to_map.collect { |attr_key|
+          builder_attribute.(attr_key)
+        }.reduce({}, :merge)
+      end
+
+      def build_hash_attribute
+        -> (object, options, attr_key) {
+          if attr_key.kind_of?(Hash)
+            key   = attr_key.keys.first
+            value = association_value(object, attr_key, options)
+          else
+            key   = attr_key
+            value = object.send(attr_key)
+          end
+          {:"#{key}" => value}
+        }
+      end
+
+      def association_value(object, attr_key, options)
+        key          = attr_key.keys.first
+        object_value = object.send(key)
+
+        return object_value unless options[:map_association]
+
+        mapper = attr_key[key][:mapper].constantize.instance
+
+        if object_value.try(:size)
+          object_value.collect do |obj|
+            mapper.send(options[:map_association_to], obj, {map_association: false})
+          end
+        else
+          mapper.send(options[:map_association_to], object_value, {map_association: false})
+        end
+      end
+
+      # --- Classes Methods ---
 
       def self.entity_class(entity_class)
         @entity_class = entity_class
@@ -53,47 +110,6 @@ module Ar
 
       def self._attrs_to_entity
         @attrs_to_entity
-      end
-
-      private
-
-      # TODO: Improve way to mapper entity to record with associations
-
-      def attributes_from(object, attrs_to_map, options={})
-        map_association = options.fetch(:map_association, true)
-        attrs_to_map ||= self.class._attributes_to_map
-        begin
-          attrs_to_map.collect { |attr_key|
-            if attr_key.kind_of?(Hash)
-              key   = attr_key.keys.first
-              value = association_value(object, attr_key, map_association)
-            else
-              key   = attr_key
-              value = object.send(attr_key)
-            end
-
-            {:"#{key}" => value}
-
-          }.reduce({}, :merge)
-        rescue => e
-          raise StandardError, "#{self.class} -> #{e.message}"
-        end
-      end
-
-      def association_value(object, attr_key, map_association)
-        key          = attr_key.keys.first
-        mapper       = attr_key[key][:mapper].constantize.instance
-        object_value = object.send(key)
-
-        return object_value unless map_association
-
-        if object_value.try(:size)
-          return object_value.collect do |obj|
-            mapper.map_to_entity(obj, {map_association: false})
-          end
-        else
-          return mapper.map_to_entity(object_value, {map_association: false})
-        end
       end
 
     end
