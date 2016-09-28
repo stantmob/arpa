644527d6a79c2402cd2d2c88f2c8f5aa2c05811c
Rachid Calazans <rachidcalazans@gmail.com>
2016-09-28 09:02:13 -0300

    Update Addition and Verifier service
    - remove session necessity from both class.
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- lib/arpa/additions/resource.rb
+++ lib/arpa/additions/resource.rb
@@ -7,12 +7,8 @@ module Arpa
       end
 
       def has_access?(resource, action)
-        return unless has_session_or_current_user?
+        return unless has_current_user?
         verifier.has_access?(resource, action)
-      end
-
-      def reset_permissions
-        verifier.reset_permissions
       end
 
       def verifier
@@ -21,12 +17,10 @@ module Arpa
 
       private
 
-      def has_session_or_current_user?
-        verified_session      = try(:session)
+      def has_current_user?
         verified_current_user = try(:current_user)
-        return true if verified_session && verified_current_user
+        return true if verified_current_user
         log = Logger.new(STDOUT)
-        log.warn("The ApplicationController must has a attribute or method 'session'") unless verified_session
         log.warn("The ApplicationController must has a attribute or method 'current_user'") unless verified_current_user
         false
       end
--- lib/arpa/services/verifier.rb
+++ lib/arpa/services/verifier.rb
@@ -2,20 +2,14 @@ module Arpa
   module Services
     class Verifier
 
-      def initialize(session, current_user)
-        @session      = session
+      def initialize(current_user)
         @current_user = current_user
       end
 
       def has_access?(resource, action)
         free_access_action = action.to_s.split('_')[0]
         return true if @current_user.is_arpa_admin? || free_access_action.empty?
-        @session[:entity_permissions] ||= action_finder.permissions(@current_user.profile_ids)
-        @session[:entity_permissions].has_permission?(resource.to_s, action.to_s)
-      end
-
-      def reset_permissions
-        @session[:entity_permissions] = nil
+        action_finder.permission(resource.to_s, action.to_s, @current_user.profile_ids)
       end
 
       private
